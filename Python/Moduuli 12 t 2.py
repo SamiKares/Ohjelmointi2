7d7ae13bcad9236327c28eb3e747c3c9a4f911e0
SamiKares <sami.karesniemi@gmail.com>
2024-10-31 14:01:34 +0200

    Moduuli  12 tehtävät 1-2

--- Python/Moduuli 12 t 2.py
+++ Python/Moduuli 12 t 2.py
@@ -3,5 +3,4 @@ import json
 kaupunki = input("Anna kaupunki")
 tiedustelu =f"https://api.openweathermap.org/data/2.5/weather?q={kaupunki}&lang=fi&appid=43f03023725685797c211f067be0bfe8&units=metric"
 vastaus = requests.get(tiedustelu).json()
-#print(json.dumps(vastaus, indent=2))
 print(f'Sää kaupungissa: {vastaus["name"]}, tällä hetkellä: {vastaus["weather"][0]["description"]} ja lämpötila: {vastaus["main"]["temp"]} celsiusta')
